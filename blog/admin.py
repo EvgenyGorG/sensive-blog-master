c18c037e4d42e44852f1c3bafb7184d2a517af33
Evgeny <evgenygorokhovg@yahoo.com>
2025-07-25 16:35:03 +0300

    Registrars are used.

--- blog/admin.py
+++ blog/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
-
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     raw_id_fields = ['author', 'post']
     list_display = ['author', 'post', 'published_at']
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['tags', 'likes']
     list_display = ['title', 'published_at']
 
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(Tag)
-admin.site.register(Comment, CommentAdmin)
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
